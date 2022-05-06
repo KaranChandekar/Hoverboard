44b2cad733f7ce3ebdeb1f77531bd3ababf47b33
Karan Chandekar <karanchandekar431@gmail.com>
2022-05-06 22:46:55 +0530

    function to set colors

--- script.js
+++ script.js
@@ -12,3 +12,9 @@ for (let i = 0; i < SQUARES; i++) {
 
   container.appendChild(square);
 }
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.background = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
